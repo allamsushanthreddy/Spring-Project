36fb7fc8a6bb58a525acb9efb73417eb1a1b69bf
allamsushanthreddy <100283131+allamsushanthreddy@users.noreply.github.com>
2022-10-25 18:38:33 +0530

    Add files via upload

---
+++ Test/src/main/java/com/greatlearning/customer/service/CustomerService.java
@@ -0,0 +1,18 @@
+package com.greatlearning.customer.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.greatlearning.customer.entity.Customer;
+
+@Service
+public interface CustomerService {
+	public List<Customer> findAll();
+	
+	public Customer findById(int theId);
+	
+	public void save(Customer thecustomer);
+	
+	public void deleteById(int theId);
+}
